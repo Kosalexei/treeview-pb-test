bae9b8302387bcb40c840a752e5cea8f356fd379
Alexei Kostyuchenko <kos_ae@outlook.com>
2019-10-14 18:58:32 +0700

    Add treeview component

---
+++ src/components/Treeview.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="treeview">
+    <table class="treeview__table">
+      <thead class="treeview__table-head">
+        <tr>
+          <th v-for="header in headers" :key="header.value">{{header.label}}</th>
+        </tr>
+      </thead>
+      <tbody class="treeview__table-body">
+        <tr v-for="(item, rIndex) in items" :key="rIndex">
+          <td v-for="(header, dIndex) in headers" :key="dIndex">{{header.value ? item[header.value] : null}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  data: () => ({
+    headers: [
+      {
+        label: "Наименование",
+        value: "name"
+      },
+      {
+        label: "Тип",
+        value: "type"
+      },
+      {
+        label: "Дата создания",
+        value: "created"
+      },
+      {
+        label: "Дата изменения",
+        value: "modified"
+      }
+    ],
+
+    items: [
+      {
+        name: "Test",
+        type: "Папка с файлами",
+        created: "14.10.2019",
+        modified: "14.10.2019"
+      },
+
+      {
+        name: "Test",
+        type: "Файл",
+        created: "14.10.2019",
+        modified: "14.10.2019"
+      },
+
+      {
+        name: "Test",
+        type: "Директория",
+        created: "14.10.2019",
+        modified: "14.10.2019"
+      },
+
+      {
+        name: "Test",
+        type: "Директория",
+        created: "14.10.2019",
+        modified: "14.10.2019"
+      }
+    ]
+  }),
+
+  methods: {}
+};
+</script>

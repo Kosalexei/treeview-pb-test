ed0130a044db8aba711e5ce3fd8cd6cd5e6c777a
Kosalexei <kos_ae@outlook.com>
2019-10-14 23:55:27 +0700

    Server interaction

--- src/components/Treeview.vue
+++ src/components/Treeview.vue
@@ -1,75 +1,112 @@
 <template>
   <div class="treeview">
+    <div class="treeview__actions">
+      <button @click="addDirectoryModal = true">Добавить директорию</button>
+    </div>
+
     <table class="treeview__table">
       <thead class="treeview__table-head">
-        <tr>
-          <th v-for="header in headers" :key="header.value">{{header.label}}</th>
-        </tr>
+      <tr>
+        <th v-for="header in headers" :key="header.value">{{header.label}}</th>
+      </tr>
       </thead>
       <tbody class="treeview__table-body">
-        <tr v-for="(item, rIndex) in items" :key="rIndex">
-          <td v-for="(header, dIndex) in headers" :key="dIndex">{{header.value ? item[header.value] : null}}</td>
-        </tr>
+      <tr v-if="parseInt(dirId) !== 0" @click="getDirectory(parentID)">
+        <td>...</td>
+      </tr>
+      <tr v-for="(item, rIndex) in items" :key="rIndex" @click="getDirectory(item.ID, item.ParentID)">
+        <td v-for="(header, dIndex) in headers" :key="dIndex">{{header.value ? item[header.value] : null}}</td>
+      </tr>
       </tbody>
     </table>
+
+    <modal v-model="addDirectoryModal" title="Добавить директорию">
+      <template v-slot:body>
+        <form @submit.prevent="addDirectory()">
+          <label for="dir-name"></label>
+          <input id="dir-name" v-model="dirName">
+        </form>
+      </template>
+      <template v-slot:action>
+        <button @click="addDirectory()">Добавить</button>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
-export default {
-  props: {},
+    import Modal from "./Modal";
 
-  data: () => ({
-    headers: [
-      {
-        label: "Наименование",
-        value: "name"
-      },
-      {
-        label: "Тип",
-        value: "type"
-      },
-      {
-        label: "Дата создания",
-        value: "created"
-      },
-      {
-        label: "Дата изменения",
-        value: "modified"
-      }
-    ],
+    export default {
+        components: {Modal},
 
-    items: [
-      {
-        name: "Test",
-        type: "Папка с файлами",
-        created: "14.10.2019",
-        modified: "14.10.2019"
-      },
+        props: {},
 
-      {
-        name: "Test",
-        type: "Файл",
-        created: "14.10.2019",
-        modified: "14.10.2019"
-      },
+        data: () => ({
+            addDirectoryModal: false,
 
-      {
-        name: "Test",
-        type: "Директория",
-        created: "14.10.2019",
-        modified: "14.10.2019"
-      },
+            dirId: 0,
 
-      {
-        name: "Test",
-        type: "Директория",
-        created: "14.10.2019",
-        modified: "14.10.2019"
-      }
-    ]
-  }),
+            parentID: 0,
 
-  methods: {}
-};
+            dirName: "",
+
+            headers: [
+                {
+                    label: "Наименование",
+                    value: "Name"
+                },
+                {
+                    label: "Тип",
+                    value: "Type"
+                },
+                {
+                    label: "Дата создания",
+                    value: "Created"
+                },
+                {
+                    label: "Дата изменения",
+                    value: "Modified"
+                }
+            ],
+
+            items: []
+        }),
+
+        mounted() {
+            this.getDirectory(this.dirId);
+        },
+
+        methods: {
+            addDirectory() {
+                const data = {dir_name: this.dirName, parent_id: this.dirId, dir_description: "Desc"};
+
+                this.$http({method: "post", url: "/directory", data}).then(({data}) => {
+                    if (!data.data) return;
+                    const _data = data.data;
+                    this.items = _data.items;
+                    this.dirId = _data.dirId;
+                    this.addDirectoryModal = false;
+                    this.dirName = "";
+                }).catch(e => {
+                    console.log(e)
+                })
+            },
+
+            getDirectory(id, prarentID) {
+                const params = {parent_id: id};
+                this.parentID = prarentID;
+
+                this.$http({method: "get", url: "/directory", params}).then(({data}) => {
+                    if (!data.data) return;
+                    const _data = data.data;
+                    this.dirId = _data.dirId;
+                    this.items = _data.items;
+
+                }).catch(e => {
+                    console.log(e)
+                })
+            }
+        }
+    };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/scss/app.scss";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+console.log(process.env)
+Vue.prototype.$http = axios.create({
+    baseURL: process.env.VUE_APP_API_HOST,
+    timeout: 30000 // request timeout
+});
+
 
 new Vue({
   render: h => h(App)
